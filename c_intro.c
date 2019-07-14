b0a149828e0be31a2e5916d1318e4ec72e18d19b
Connors <connors@HSIMSs-MacBook-Pro.local>
2019-07-14 21:20:31 -0230

    removed comments

--- c_intro.c
+++ c_intro.c
@@ -3,30 +3,6 @@
 void swap(int *x, int *y);
 
 int main(void){
-	// int x = 7;
-	// printf("x is %d\n", x);
-	// x = 14;
-	// printf("x is %d\n", x);
-
-	// int *aptr = &x;
-	// printf("%x\n", aptr);
-	// printf("x is %d\n", *aptr);
-	// *aptr = 21;
-	// printf("x is now %d\n", x);
-
-	// int somenumbers[5];
-	// int *ptr = somenumbers;
-	// int v = 2;
-
-	// for(;ptr<&somenumbers[5];ptr++){
-	// 	*ptr = v;
-	// 	v = v * 2;
-	// }
-
-	// int i;
-	// for(i = 0; i < 5; i++){
-	// 	printf("%d\n", somenumbers[i]);
-	//}
 
 	int a = 5;
 	int b = 8;
